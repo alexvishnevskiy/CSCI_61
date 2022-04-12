ed08ba810ad9e346a7371a3f9b4bdbf4eb394b2a
alexvishnevskiy <mrflowy043@gmail.com>
2022-04-11 18:49:56 -0700

    Week 3 seminar 1

---
+++ week03/seminar01/README.md
@@ -0,0 +1,12 @@
+## Merge sort
+
+### Vizualization
+![mergesort](./mergesort.png)
+### Merging
+![last_elment](./last_element.png)
+![left_right](./left_right.png)
+### Runtime
+T(N) = 2T(N/2) + O(N)
+![runtime](./runtime.jpg)
+### Example
+![visualize](./visualize.jpg)
---
+++ week03/seminar01/mergeSort.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+using namespace std;
+
+
+void merge(vector<int>& A, int start, int middle, int end){
+    vector<int> L, R;
+    for (int i = start; i <= middle; i++){
+        L.push_back(A[i]);
+    }
+    L.push_back(INT_MAX); //should use additional cell, to avoid problem
+    // if L finish first it should use this value to compare with right
+
+    for (int j = middle+1; j <= end; j++){
+        R.push_back(A[j]);
+    }
+    R.push_back(INT_MAX);
+
+    int i = 0, j = 0;
+    for (int k = start; k <= end; k++){
+        if (L[i] < R[j]){
+            A[k] = L[i];
+            i++;
+        }else{
+            A[k] = R[j];
+            j++;
+        }
+    }
+}
+
+void mergeSort(vector<int>& A, int start, int end){
+    if (start == end){
+        return;
+    }else{
+        int middle = (start + end)/2;
+        mergeSort(A, start, middle);
+        mergeSort(A, middle+1, end);
+        merge(A, start, middle, end);
+    }
+}
+
+void mergeSort(vector<int>& A){
+    mergeSort(A, 0, A.size()-1);
+}
