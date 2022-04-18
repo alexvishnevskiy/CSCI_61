e3f7bd619319577837761b31e0d81ae58450b3d0
alexvishnevskiy <mrflowy043@gmail.com>
2022-04-18 12:59:16 -0700

    Add heap realisation

--- week03/seminar02/quickSort.cpp
+++ week03/seminar02/quickSort.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <iostream>
+using namespace std;
 
 
 int partition(vector<int>& A, int start, int end){
@@ -6,12 +8,12 @@ int partition(vector<int>& A, int start, int end){
     int pivot_index = start;
     int i = start + 1; // we can pick another pivot, for example median
     int j = end;
-
+    
     while (i <= j) {
         while ((i <= end) && (A[i] <= pivot)){
             i++;
         }
-        while ((j >= start) && A[j] > pivot)){
+        while ((j >= start) && (A[j] > pivot)){
             j--;
         }
         if (i < j){
@@ -26,8 +28,27 @@ void QuickSort(vector<int>& A, int start, int end){
     // if (start + 10 <= end) insertionSort(A, start, end); 
     // if there are 10 elements it is likely that list is nearly sorted
     // and it is better to use another algorithm
-    if (start == end) return;
-    int pivot_index = partition(A, start, end);
-    QuickSort(A, start, pivot_index-1);
-    QuickSort(A, pivot_index, end);
+    if (start == end){
+        return;
+    }else{
+        int pivot_index = partition(A, start, end);
+        QuickSort(A, start, pivot_index-1);
+        QuickSort(A, pivot_index, end);
+    }
+}
+
+
+int main(){
+    vector<int> a;
+
+    a.push_back(0);
+    a.push_back(14);
+    a.push_back(1);
+    a.push_back(2);
+    QuickSort(a, 0, 3);
+
+    // for (int x: a){
+    //     cout << x << " ";
+    // }
+    return 0;
 }
