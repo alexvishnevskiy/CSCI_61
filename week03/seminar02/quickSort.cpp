78468658971b6d262a52ac1850c82cba18a32bc7
alexvishnevskiy <mrflowy043@gmail.com>
2022-05-07 14:07:26 -0700

    Add notes for quicksort

--- week03/seminar02/quickSort.cpp
+++ week03/seminar02/quickSort.cpp
@@ -13,13 +13,13 @@ int partition(vector<int>& A, int start, int end){
     int j = end;
     
     while (i <= j) {
-        while ((i <= end) && (A[i] <= pivot)){
+        while ((i <= end) && (A[i] <= pivot)){ // check for condition and also the end of array
             i++;
         }
-        while ((j >= start) && (A[j] > pivot)){
+        while ((j >= start) && (A[j] > pivot)){ // check for condition and also the start of array
             j--;
         }
-        if (i < j){
+        if (i < j){ // if we skip this condition, we can break the order
             swap(A[i], A[j]);
         }
     }
@@ -31,7 +31,8 @@ void QuickSort(vector<int>& A, int start, int end){
     // if (start + 10 <= end) insertionSort(A, start, end); 
     // if there are 10 elements it is likely that list is nearly sorted
     // and it is better to use another algorithm
-    if (start >= end){
+    if (start <= end){ // when two element there is a case when partion - 1 < start
+                       // for example [0, 14]
         return;
     }else{
         int pivot_index = partition(A, start, end);
@@ -44,11 +45,11 @@ void QuickSort(vector<int>& A, int start, int end){
 int main(){
     vector<int> a;
 
-    a.push_back(0);
     a.push_back(14);
-    a.push_back(1);
-    a.push_back(2);
-    QuickSort(a, 0, 3);
+    a.push_back(0);
+    // a.push_back(1);
+    // a.push_back(2);
+    QuickSort(a, 0, 1);
 
     // for (int x: a){
     //     cout << x << " ";
